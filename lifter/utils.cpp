9548649920352664fcd90f9bdc9317c79e65ab0b
G0lge <yusufcan_31@hotmail.com>
2024-05-13 17:12:25 +0300

    fix filereading logic

--- lifter/utils.cpp
+++ lifter/utils.cpp
@@ -18,7 +18,7 @@ uintptr_t RvaToFileOffset(win::nt_headers_x64_t* ntHeaders, uint32_t rva) {
 	auto sectionHeader = ntHeaders->get_sections();
 	for (int i = 0; i < ntHeaders->file_header.num_sections; i++, sectionHeader++) {
 		if (rva >= sectionHeader->virtual_address && rva < (sectionHeader->virtual_address + sectionHeader->virtual_size)) {
-			if (sectionHeader->characteristics.mem_read && !sectionHeader->characteristics.mem_write) // remove?
+			if (sectionHeader->characteristics.mem_execute || (sectionHeader->characteristics.mem_read && !sectionHeader->characteristics.mem_write) ) // remove?
 				return rva - sectionHeader->virtual_address + sectionHeader->ptr_raw_data;
 			else
 				return 0;
