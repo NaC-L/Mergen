f12581035a8163584f815ed740aadc4d73bb29d3
G0lge <yusufcan_31@hotmail.com>
2024-06-29 06:51:13 +0300

    remove unused code

--- lifter/PathSolver.h
+++ lifter/PathSolver.h
@@ -1,14 +1,6 @@
 #pragma once
 #include "includes.h"
 
-void test_optxd(Function* clonedFuncx);
-
 void final_optpass(Function* clonedFuncx);
-
-opaque_info isOpaque(Function* function);
-
-ROP_info isROP(Function* function, BasicBlock& clonedBB, uintptr_t& dest);
-
-JMP_info isJOP(Function* function, uintptr_t& dest);
 
 PATH_info solvePath(Function* function, uintptr_t& dest, string debug_filename);
