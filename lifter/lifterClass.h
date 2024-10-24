c6b979344003902b926d98bb6a6f13e12cc6c4fe
naci <yusufcan_31@hotmail.com>
2024-10-25 00:12:11 +0300

    test crash reason2

--- lifter/lifterClass.h
+++ lifter/lifterClass.h
@@ -49,7 +49,7 @@ struct InstructionKey {
     }
 
     static inline InstructionKey getTombstoneKey() {
-      return InstructionKey(nullptr, static_cast<Value*>(nullptr));
+      return InstructionKey(nullptr, static_cast<Value*>(-1));
     }
   };
 };
