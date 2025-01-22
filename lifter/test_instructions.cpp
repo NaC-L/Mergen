0248538b707d069122ec2f8b4a65013218910156
naci <yusufcan_31@hotmail.com>
2025-01-22 17:09:35 +0300

    Update test_instructions.cpp

--- lifter/test_instructions.cpp
+++ lifter/test_instructions.cpp
@@ -42,7 +42,7 @@ int testInit() {
   const std::string block_name = "entry";
   auto bb = llvm::BasicBlock::Create(context, block_name.c_str(), function);
 
-  llvm::IRBuilder<>(bb);
+  llvm::IRBuilder<> builder = llvm::IRBuilder<>(bb, Folder);
 
   lifterClass* main = new lifterClass(builder, 0x133700);
 
