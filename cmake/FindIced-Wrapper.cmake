5e5f68f0b6016306c3ab5f59fff46bc3cf7852a0
naci <29066657+NaC-L@users.noreply.github.com>
2025-04-24 05:47:29 +0300

    build without iced if rust not found

--- cmake/FindIced-Wrapper.cmake
+++ cmake/FindIced-Wrapper.cmake
@@ -1,3 +1,12 @@
+find_program(CARGO_EXECUTABLE cargo)
+
+if (CARGO_EXECUTABLE)
+    message("Cargo not found. Default to Zydis")
+    return()
+endif()
+
+message("Cargo not found. Default to Iced")
+
 include(FetchContent)
 
 FetchContent_Declare(
@@ -9,3 +18,7 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(Corrosion)
 
 corrosion_import_crate(MANIFEST_PATH icpped_rust/Cargo.toml)
+
+
+set(ICED_FOUND TRUE CACHE BOOL "Rust/Cargo found => build the iced (Rust) backend")
+add_compile_definitions(ICED_FOUND)
