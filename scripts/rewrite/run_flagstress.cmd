567e0d7daf8d6c6ab61e8445a061019c0db9850e
yusufcanislek <yusuf.canislek@meetdandy.com>
2026-03-03 23:04:21 +0300

    Add rewrite regression automation, vectors, and documentation

---
+++ scripts/rewrite/run_flagstress.cmd
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+call "%~dp0generate_flag_stress_vectors.cmd"
+if errorlevel 1 exit /b 1
+
+set "MERGEN_TEST_VECTORS=%~dp0..\..\lifter\test_vectors\oracle_vectors_flagstress.json"
+set "SKIP_ORACLE_GENERATION=1"
+set "MERGEN_TEST_CHECK_FLAGS=1"
+
+call "%~dp0run_microtests.cmd" %*
+exit /b %errorlevel%
