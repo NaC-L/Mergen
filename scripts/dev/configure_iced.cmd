67c26c9bab3a02589122804d40ddd2b620e76976
yusufcanislek <yusuf.canislek@meetdandy.com>
2026-04-03 20:28:26 +0300

    Fix iced CI cargo detection

--- scripts/dev/configure_iced.cmd
+++ scripts/dev/configure_iced.cmd
@@ -1,11 +1,24 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 rem --- clang-cl auto-detects MSVC headers/libs; no VsDevCmd needed ---
 
-:resolve_cargo
-set "CARGO_BIN=%USERPROFILE%\.cargo\bin"
-if exist "%CARGO_BIN%\cargo.exe" set "PATH=%CARGO_BIN%;%PATH%"
+::resolve_cargo
+set "CARGO_BIN="
+set "CARGO_EXE="
+if defined CARGO_EXECUTABLE if exist "%CARGO_EXECUTABLE%" set "CARGO_EXE=%CARGO_EXECUTABLE%"
+if not defined CARGO_EXE if defined CARGO_HOME if exist "%CARGO_HOME%\bin\cargo.exe" set "CARGO_EXE=%CARGO_HOME%\bin\cargo.exe"
+if not defined CARGO_EXE if defined USERPROFILE if exist "%USERPROFILE%\.cargo\bin\cargo.exe" set "CARGO_EXE=%USERPROFILE%\.cargo\bin\cargo.exe"
+if defined CARGO_EXE for %%I in ("%CARGO_EXE%") do set "CARGO_BIN=%%~dpI"
+if defined CARGO_BIN set "PATH=%CARGO_BIN%;%PATH%"
+set "CMAKE_CARGO_ARG="
+if defined CARGO_EXE set "CMAKE_CARGO_ARG=-DCARGO_EXECUTABLE=%CARGO_EXE%"
+set "RUST_TOOLCHAIN_ARG="
+if defined CARGO_EXE (
+    for /f "tokens=1" %%I in ('rustup show active-toolchain 2^>nul') do set "MERGEN_RUST_TOOLCHAIN=%%I"
+    if defined MERGEN_RUST_TOOLCHAIN set "RUST_TOOLCHAIN_ARG=-DRust_TOOLCHAIN=!MERGEN_RUST_TOOLCHAIN!"
+    set "MERGEN_RUST_TOOLCHAIN="
+ )
 
 :resolve_cmake
 set "CMAKE_BIN="
@@ -39,6 +52,6 @@ if not defined MERGEN_C_COMPILER set "MERGEN_C_COMPILER=clang-cl"
 set "MERGEN_CXX_COMPILER=%CMAKE_CXX_COMPILER%"
 if not defined MERGEN_CXX_COMPILER set "MERGEN_CXX_COMPILER=%MERGEN_C_COMPILER%"
 
-:configure
-"%CMAKE_BIN%" -G Ninja -S "%REPO_ROOT%" -B "%REPO_ROOT%\build_iced" -DCMAKE_BUILD_TYPE=Release -DLLVM_DIR="%LLVM_CMAKE_DIR%" -DCMAKE_C_COMPILER="%MERGEN_C_COMPILER%" -DCMAKE_CXX_COMPILER="%MERGEN_CXX_COMPILER%" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+::configure
+"%CMAKE_BIN%" -G Ninja -S "%REPO_ROOT%" -B "%REPO_ROOT%\build_iced" -DCMAKE_BUILD_TYPE=Release -DLLVM_DIR="%LLVM_CMAKE_DIR%" -DCMAKE_C_COMPILER="%MERGEN_C_COMPILER%" -DCMAKE_CXX_COMPILER="%MERGEN_CXX_COMPILER%" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON %CMAKE_CARGO_ARG% %RUST_TOOLCHAIN_ARG%
 exit /b %errorlevel%
